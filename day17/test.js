0fe2d14acec550a73ce5938233148668c0c08751
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2017-12-17 19:50:52 -0500

    Day 17

--- day17/test.js
+++ day17/test.js
@@ -1,10 +1,5 @@
 import {getValueAfter2017} from './task01';
-import {getValueAfter0} from './task01';
 
 test('task 01 example', () => {
     expect(getValueAfter2017(3)).toBe(638);
-});
-
-test('task 02 example', () => {
-    // expect(getValueAfter0(3)).toBe(309);
 });
